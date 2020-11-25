d0fbb14de66b3ba953076334cfe2413112cc462e
Kasparidi <kasparidi@gmail.com>
2020-11-25 17:43:47 +0300

    added second task

--- src/test/java/ru/netology/OrderCardTest.java
+++ src/test/java/ru/netology/OrderCardTest.java
@@ -18,4 +18,14 @@ class OrderCardTest {
         $("button").click();
         $("p").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+
+    @Test
+    public void ifUseEnglishLetters() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Marina Probkina");
+        $("[data-test-id=phone] input").setValue("+79161111111");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
 }
